e3604a1950c273e734a8b423415e9260f9f00b85
Sergei Zotov <zsv.main@gmail.com>
2021-12-05 08:11:11 +0500

    feat-4: Added theming by components config. First example

--- src/modules/Laptops/Laptops.tsx
+++ src/modules/Laptops/Laptops.tsx
@@ -1,4 +1,10 @@
+import { themeLaptops } from "utils/theming/theming";
+
 const Laptops = () => {
   return <div>Laptops</div>;
+};
+
+Laptops.config = {
+  Theme: themeLaptops,
 };
 export default Laptops;
